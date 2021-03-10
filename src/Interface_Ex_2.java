676f55f94d2d58d4731b0e4a189df40100f05775
kirtan <kirtanpanchal700@gmail.com>
2021-03-10 18:14:59 +0530

    Commi Data first time

--- src/Interface_Ex_2.java
+++ src/Interface_Ex_2.java
@@ -1,18 +1,19 @@
-import java.util.Collections;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Interface_Ex_2 {
     public static void main(String[] args) {
-        Set set
-        for (String s : set)
-        {
-            
-        }
+        Set set=new HashSet();
+
+        set.add("kirtan");
+        set.add("pacnhal");
+        set.add("aca");
+        set.add("kn");
+        System.out.println(sortingSet(set));
     }
-    public static void sortingSet(Set set)
+
+    public static Set sortingSet(Set set)
     {
-        SortedSet<String> set=new TreeSet<String>();
+        SortedSet<String> s=new TreeSet<String>(set);
+        return s;
     }
 }
