998ebf1a708642e27534696707568ed88ed13b4b
kirtan2312 <30954062+kirtan2312@users.noreply.github.com>
2021-03-11 16:46:38 +0530

    Update Agg_Fun_Ex_2.java

--- src/Agg_Fun_Ex_2.java
+++ src/Agg_Fun_Ex_2.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Agg_Fun_Ex_2 {
     public static void main(String[] args) {
         List<Album> list=new ArrayList<Album>();
         Album a1=new Album();
-        a1.setName("a");
+        a1.setName("c");
         a1.setRating(4);
         list.add(a1);
 
@@ -15,12 +15,14 @@ public class Agg_Fun_Ex_2 {
         list.add(a2);
 
         Album a3=new Album();
-        a3.setName("c");
+        a3.setName("a");
         a3.setRating(5);
         list.add(a3);
 
-        //get albums which has 4 and above ratings
-        list.stream().filter(a->(a.getRating()>=4)).forEach(a->System.out.println(a.getName()));
 
+        Set<String> s=new TreeSet<String>();
+        //get albums which has 4 and above ratings
+        list.stream().filter(a->(a.getRating()>=4)).forEach(a->s.add(a.getName()));
+        System.out.println(s);
     }
 }
