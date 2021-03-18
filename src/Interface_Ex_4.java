2bb492ed57ea35880e2da1d59e9baf2c75d8a64f
kirtan2312 <30954062+kirtan2312@users.noreply.github.com>
2021-03-18 14:57:21 +0530

    Update Interface_Ex_4.java

--- src/Interface_Ex_4.java
+++ src/Interface_Ex_4.java
@@ -3,23 +3,23 @@ import java.util.*;
 public class Interface_Ex_4 {
     public static void main(String[] args) {
         List<Employee> list=new ArrayList<Employee>();
-        Employee e=new Employee();
-        e.setId(1);
-        e.setFn("a");
-        e.setLn("aaa");
-        list.add(e);
+        Employee employee1=new Employee();
+        employee1.setId(1);
+        employee1.setFn("a");
+        employee1.setLn("aaa");
+        list.add(employee1);
 
-        Employee e2=new Employee();
-        e2.setId(2);
-        e2.setFn("kirtan");
-        e2.setLn("panchal");
-        list.add(e2);
+        Employee employee2=new Employee();
+        employee2.setId(2);
+        employee2.setFn("kirtan");
+        employee2.setLn("panchal");
+        list.add(employee2);
 
-        Employee e3=new Employee();
-        e3.setId(3);
-        e3.setFn("b");
-        e3.setLn("bb");
-        list.add(e3);
+        Employee employee3=new Employee();
+        employee3.setId(3);
+        employee3.setFn("b");
+        employee3.setLn("bb");
+        list.add(employee3);
 
         Employee employee=chooseRandomEmp(list);
         System.out.println(employee.getFn());
@@ -29,9 +29,10 @@ public class Interface_Ex_4 {
     public static Employee chooseRandomEmp(List<Employee> list)
     {
         Random r=new Random();
-        Employee emp=list.get(r.nextInt(list.size()));
-        return emp;
+        Employee employee=list.get(r.nextInt(list.size()));
+        return employee;
     }
 }
+
 
 o/p:-https://github.com/kirtan2312/collection/blob/kirtan/src/Snapshots/Interface_Ex_4.png
