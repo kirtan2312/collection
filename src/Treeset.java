09b7d6ea49a801524bad97e65eb42a70f1643060
kirtan2312 <30954062+kirtan2312@users.noreply.github.com>
2021-03-18 15:06:40 +0530

    Update Treeset.java

--- src/Treeset.java
+++ src/Treeset.java
@@ -5,18 +5,17 @@ public class Treeset {
     public static void main(String[] args) {
         //Treeset does not allow null element
         //not thread safe
-        TreeSet t_set=new TreeSet();
-        t_set.add(10);
-//        t_set.add("a");
-        t_set.add(20);
-        t_set.add(30);
-        Iterator it= t_set.iterator();
+        TreeSet tree_set=new TreeSet();
+        tree_set.add(10);
+//        tree_set.add("a");
+        tree_set.add(20);
+        tree_set.add(30);
+        Iterator it= tree_set.iterator();
         //once you applied iterator you can not add values
-//        t_set.add(50);//it gives concurentModification Exception
+//        tree_set.add(50);//it gives concurentModification Exception
         while(it.hasNext())
         {
             System.out.println(it.next());
         }
-
     }
 }
