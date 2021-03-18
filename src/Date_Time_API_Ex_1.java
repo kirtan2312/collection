b26a4c837875566ad0632fd44bdaa156cdbce28c
kirtan2312 <30954062+kirtan2312@users.noreply.github.com>
2021-03-18 14:27:26 +0530

    Update Date_Time_API_Ex_1.java

--- src/Date_Time_API_Ex_1.java
+++ src/Date_Time_API_Ex_1.java
@@ -1,17 +1,16 @@
 import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
+
 
 public class Date_Time_API_Ex_1 {
 
     public static void main(String[] args) {
-        int year=Integer.parseInt(args[0]);
-        Year y=Year.of(year);
+        Year year=Year.of(2020);
         for(Month month: Month.values())
         {
-            YearMonth year_month=YearMonth.of(year,month);
+            YearMonth year_month=YearMonth.of(2020,month);
             System.out.println(month.name()+":"+year_month.lengthOfMonth());
         }
     }
 }
-o/p:https://github.com/kirtan2312/collection/blob/kirtan/src/Snapshots/Date_Time_API_Ex_1.png
+
+https://github.com/kirtan2312/collection/blob/main/src/Snapshots/Date_Time_API_Ex_1.png
