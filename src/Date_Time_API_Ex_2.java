57549a06ed6edcaac9e8d8e57457451a88d5b4d2
kirtan2312 <30954062+kirtan2312@users.noreply.github.com>
2021-03-18 14:03:06 +0530

    Add files via upload

---
+++ src/Date_Time_API_Ex_2.java
@@ -0,0 +1,16 @@
+import java.time.*;
+import java.time.temporal.TemporalAdjusters;
+
+public class Date_Time_API_Ex_2 {
+    public static void main(String[] args){
+        Month month= Month.MAY;
+        LocalDate localDate=Year.now().atMonth(month).atDay(1).with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+        Month date_month=localDate.getMonth();
+        while(date_month==month)
+        {
+            System.out.println(localDate+" : "+localDate.getDayOfWeek());
+            localDate=localDate.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+            date_month=localDate.getMonth();
+        }
+    }
+}
---
+++ src/Date_Time_API_Ex_3.java
@@ -0,0 +1,21 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Date_Time_API_Ex_3 {
+    public static void main(String[] args) {
+        LocalDate localDate=LocalDate.now();
+
+        System.out.println(toCheckFriday(localDate));
+
+    }
+    public static boolean toCheckFriday(LocalDate date)
+    {
+//        date.plusDays(1);
+        System.out.println(date.getDayOfWeek());
+        if(date.getDayOfWeek()==DayOfWeek.THURSDAY)
+        {
+            return true;
+        }
+        return false;
+    }
+}
