c86dc7c703dd4869061e67d48ceacbfde2152e2c
kirtan2312 <30954062+kirtan2312@users.noreply.github.com>
2021-03-18 15:02:48 +0530

    Update Linkedlist.java

--- src/Linkedlist.java
+++ src/Linkedlist.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 public class Linkedlist {
 
     public static void main(String[] args) {
-        LinkedList<String> ll=new LinkedList<String>();
-        ll.add("a");
-        ll.add("b");
-        ll.add("c");
-        ll.add("d");
-        System.out.println(ll.contains("a"));
+        LinkedList<String> linkedList=new LinkedList<String>();
+        linkedList.add("a");
+        linkedList.add("b");
+        linkedList.add("c");
+        linkedList.add("d");
+        System.out.println(linkedList.contains("a"));
 
         //traverse the ll in descendiing order
-        Iterator<String> it=ll.descendingIterator();
+        Iterator<String> it=linkedList.descendingIterator();
         while(it.hasNext())
         {
             String s=it.next();
@@ -25,49 +25,49 @@ public class Linkedlist {
         arr.add("e");
         arr.add("f");
         arr.add("g");
-        ll.addAll(arr);
-        System.out.println(ll);
+        linkedList.addAll(arr);
+        System.out.println(linkedList);
 
 
 
 
         //LinkedList as Queue (FIFO order)
-        System.out.println(ll.poll());
-        System.out.println(ll);
-        System.out.println(ll.pop());
-        System.out.println(ll);
-        System.out.println(ll.peek());
-        System.out.println(ll.peekFirst());
-        System.out.println(ll.peekLast());
-        System.out.println(ll);
+        System.out.println(linkedList.poll());
+        System.out.println(linkedList);
+        System.out.println(linkedList.pop());
+        System.out.println(linkedList);
+        System.out.println(linkedList.peek());
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
+        System.out.println(linkedList);
 
         LinkedList<String> list=new LinkedList<String>();
 //        list.pop();//throwing Exception//if any element not found then it throws Exception
         System.out.println(list.poll());//it returns null if any element not found
         list.push("r");
 
-        ll.pollFirst();
-        ll.pollLast();
-        System.out.println(ll);
+        linkedList.pollFirst();
+        linkedList.pollLast();
+        System.out.println(linkedList);
 
         //to add elements in First
-        ll.addFirst("j");
-        ll.offerFirst("i");
-        System.out.println(ll);
+        linkedList.addFirst("j");
+        linkedList.offerFirst("i");
+        System.out.println(linkedList);
 
-        ll.addAll(list);
-        System.out.println(ll);
+        linkedList.addAll(list);
+        System.out.println(linkedList);
 
-        ll.removeFirst();
-        ll.removeLast();
-        System.out.println(ll);
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        System.out.println(linkedList);
 
         //cerate clone of LinkedList
-        LinkedList l2=(LinkedList)ll.clone();
+        LinkedList l2=(LinkedList)linkedList.clone();
         System.out.println(l2);
 
-        ll.add(2,"j");
-        System.out.println(ll.lastIndexOf("j"));
+        linkedList.add(2,"j");
+        System.out.println(linkedList.lastIndexOf("j"));
 
     }
 }
